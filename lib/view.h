65b3b78d9a694b491e7179e33e44cacf1ec7931c
Gabriel Quintino <pikachurando@protonmail.com>
2023-09-22 22:46:29 -0300

    feat: very basic menu selection

--- lib/view.h
+++ lib/view.h
@@ -1,21 +1,60 @@
 #include <ncurses.h>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void draw_file(string file_name) {
+void draw_centered_line(WINDOW *window, string line, int y = 0) {
+  int w, h;
+
+  getmaxyx(window, h, w);
+
+  int x = w / 2;
+
+  move(y, x - (line.length() / 2));
+
+  printw(line.c_str());
+}
+
+/**
+ * Draws the contents of the file to the screen.
+ * The `x` coordinate is realtive to half the line's length.
+ *
+ * @returns the `y` position.
+ */
+int draw_centered_file(WINDOW *window, string file_name, int y = 0) {
   ifstream file (file_name);
 
   if (file.is_open()) {
     string line;
 
+    int n = y;
+
     while (getline(file, line)) {
-      printw(line.c_str());
-      printw("\n");
+      draw_centered_line(window, line, n);
+
+      n++;
     }
 
     file.close();
+
+    return n;
   }
+
+  return y;
+}
+
+int draw_menu(WINDOW *window, int pointer, vector<string> options, int y) {
+  int i;
+
+  for (i = 0; i < options.size(); i++) {
+    string r = i == pointer ? "[" : "";
+    string l = i == pointer ? "]" : "";
+
+    draw_centered_line(window, r + options[i] + l, y + i);
+  }
+
+  return y + i;
 }
 
--- main.cpp
+++ main.cpp
@@ -2,35 +2,59 @@
 #include <fstream>
 #include <string>
 #include <ncurses.h>
+#include <vector>
 
 #include "lib/view.h"
 
-#define __KEY_CLOSE 27
+#define __KEY_UP 119 // W
+#define __KEY_DOWN 115 // S
+#define __KEY_CLOSE 27 // ESC
+#define __KEY_CONFIRM 10 // ENTER
 
 using namespace std;
 
 int main () {
   bool running = true;
 
-  int c, w, h;
+  int c, y;
+
+  int pointer = 0;
 
   WINDOW *main = initscr();
+
+  curs_set(0);
+
+  vector<string> menu = {
+    "start",
+    "continue",
+    "close"
+  };
   
   while (running) {
     clear();
 
-    draw_file("misc/banner.txt");
+    y = draw_centered_file(main, "misc/banner.txt", 1);
+
+    y = draw_menu(main, pointer, menu, y + 2);
 
     c = getch();
 
-    getmaxyx(main, h, w);
+    switch (c) {
+      case __KEY_CLOSE:
+        running = false;
+        break;
 
-    running = false;
+      case __KEY_UP:
+        if (pointer > 0) pointer--;
+        break;
+
+      case __KEY_DOWN:
+        if (pointer < menu.size()) pointer++;
+        break;
+    }
   }
 
   endwin();
-
-  cout << endl << w << endl << h;
 
   return 0;
 }
